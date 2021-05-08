585b03b74a558a2f0bc8c1ff2bcb6b4a87effce7
elio <shin.yulin0516@gmail.com>
2021-05-08 23:25:15 +0800

    Initial Commit

---
+++ TrvelDiscover/TrvelDiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrvelDiscoverApp.swift
+//  TrvelDiscover
+//
+//  Created by yulin on 2021/5/8.
+//
+
+import SwiftUI
+
+@main
+struct TrvelDiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
